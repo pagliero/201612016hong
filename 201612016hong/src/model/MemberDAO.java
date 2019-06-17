fad7dfaea7797577cd874609b44221fc2c5e3a23
Hong <Hong@DESKTOP-SMQ39OU>
2019-06-17 22:53:21 +0900

    기말고사준비

---
+++ 201612016hong/src/model/MemberDAO.java
@@ -0,0 +1,127 @@
+package model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Member;
+import model.MemberFileWriter;
+
+public class MemberDAO {
+	private ArrayList<Member> memberList = null;
+	
+	private File file = null;
+	private MemberFileReader fr = null;
+	private MemberFileWriter fw = null;
+	
+	public MemberDAO(File file) {	
+		this.file = file;
+		try {
+			fr = new MemberFileReader(file);
+			memberList = fr.readMember();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public ArrayList<Member> selectAll() { 
+		// 입력한 메모리 상에 존재하는 모든 멤버 정보를 가져와 출력			
+		return memberList;	
+	}
+	
+	public Member selectMember(Member member) {
+		int index = -1;
+		if((index = searchByID(member)) >= 0)
+			return memberList.get(index);
+		else
+			return null;
+	}
+	public List<Member> searchByAddress(String address) { 
+		// 검색 결과를 저장할 ArrayList 형 객체 생성
+		List<Member> searched = new ArrayList<Member>();
+		for(Member m : memberList) {
+			if(m.getAddress().equals(address)) {
+				searched.add(m); // 검색된 정보를 추가함
+			}
+			// 검색이 안된 경우 스킵
+		}				
+		return searched;
+	}
+	
+	public List<Member> searchByName(String Name) { 
+		// 검색 결과를 저장할 ArrayList 형 객체 생성
+		List<Member> searched = new ArrayList<Member>();
+		for(Member m : memberList) {
+			if(m.getName().equals(Name)) {
+				searched.add(m); // 검색된 정보를 추가함
+			}
+			// 검색이 안된 경우 스킵
+		}				
+		return searched;
+	}
+	
+	public int searchByID(Member member) { 
+		int ret = -1; // ret가 0 이상이면 검색 성공, -1 이면 검색 실패
+		int index = 0;
+		for(Member m : memberList) {
+			if(m.getEmail().equals(member.getEmail())) {
+				ret = index;
+				break;
+			}
+			index++;
+		}				
+		return ret;
+	}
+	
+	public int insert(Member member) {
+		int ret = -1;
+		try {
+			int index = searchByID(member);
+			if(index < 0) { // -1이면 검색 실패, 등록 가능함
+				fw = new MemberFileWriter(file);
+				memberList.add(member);
+				fw.saveMember(memberList);
+				ret = 0;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		return ret;
+	}
+	
+	public int update(Member member) {
+		int ret = -1;
+		try {
+			int index = searchByID(member);
+			if(index > 0) { // -1이면 검색 실패, 등록 가능함
+				fw = new MemberFileWriter(file);
+				memberList.set(index, member);
+				fw.saveMember(memberList);
+				ret = 0;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		return ret;
+	}	
+	public int delete(Member member) {		
+		int ret = -1;
+		try {
+			int index = searchByID(member);
+			if(index >= 0) { // -1이면 검색 실패, 등록 가능함
+				fw = new MemberFileWriter(file);
+				memberList.remove(member);
+				fw.saveMember(memberList);
+				ret = 0;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		return ret;
+	}
+	public void printMemberList() {
+		for(Member m : memberList)
+			System.out.println(m.getEmail() + ":" + m.getName());
+	}
+}
---
+++ 201612016hong/src/view/MemberViewController.java
@@ -0,0 +1,274 @@
+package view;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import application.Main;
+import controller.MemberService;
+import controller.MemberServiceImpl;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import model.Member;
+import java.util.Calendar;
+
+public class MemberViewController implements Initializable {
+	@FXML	private Button btnCreate;
+	@FXML	private Button btnUpdate;
+	@FXML	private Button btnDelete;
+	
+	@FXML	private RadioButton male;
+	@FXML	private RadioButton female;
+
+	@FXML	private TextArea taFindResult;
+	@FXML	private Button btnFindByName;
+	@FXML	private Button btnFindByAddress;
+	@FXML	private TextField tfFindCondition;
+	
+	@FXML	private TextField tfEmail;
+	@FXML	private PasswordField tfPw;
+	@FXML	private TextField tfName;
+	@FXML	private TextField tfBirth;
+	@FXML	private TextField tfAddress;
+	@FXML	private TextField tfContact;
+	
+	@FXML 	private TableView<Member> tableViewMember;
+	@FXML	private TableColumn<Member, String> columnEmail;
+	@FXML	private TableColumn<Member, String> columnName;	
+	@FXML	private TableColumn<Member, String> columnPw;
+	@FXML	private TableColumn<Member, String> columnBirth;
+	@FXML	private TableColumn<Member, String> columnAge;
+	@FXML	private TableColumn<Member, String> columnAddress;
+	@FXML	private TableColumn<Member, String> columnContact;
+	@FXML	private TableColumn<Member, String> columnGender;
+	
+	// Member : model이라고도 하고 DTO, VO 라고도 함
+	// 시스템 밖에 저장된 정보를 객체들간에 사용하는 정보로 변환한 자료구조 또는 객체
+	private final ObservableList<Member> data = FXCollections.observableArrayList();
+	
+	ArrayList<Member> memberList; //회원 정보 목록 : 이중연결리스트는 아니지만 리스트의 특징과 배열 특징을 잘 혼용해 놓은 클래스 ArrayList 
+	MemberService memberService;
+	
+	public MemberViewController() {
+		
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		memberService = new MemberServiceImpl();
+		// 람다식 : java 8  함수형 언어 지원 
+		
+		columnEmail.setCellValueFactory(cvf -> cvf.getValue().emailProperty());
+		columnName.setCellValueFactory(cvf -> cvf.getValue().nameProperty());
+		//columnPW.setCellValueFactory(cvf -> cvf.getValue().pwProperty());
+		columnBirth.setCellValueFactory(cvf -> cvf.getValue().birthProperty());
+		columnAge.setCellValueFactory(cvf -> cvf.getValue().ageProperty());
+		columnAddress.setCellValueFactory(cvf -> cvf.getValue().addressProperty());
+		columnContact.setCellValueFactory(cvf -> cvf.getValue().contactProperty());
+		columnGender.setCellValueFactory(cvf -> cvf.getValue().genderProperty());
+		
+		tableViewMember.getSelectionModel().selectedItemProperty().addListener(
+				(observable, oldValue, newValue) -> showMemberInfo(newValue));
+
+		//fxml안에 이벤트 잇음.
+		//btnCreate.setOnMouseClicked(event -> handleCreate());	// 이넨트 연결은 1.initialize 2. fxml 둘 다 가능하다.
+		//btnUpdate.setOnMouseClicked(event -> handleUpdate());	// 이넨트 연결은 1.initialize 2. fxml 둘 다 가능하다.
+		
+		btnFindByAddress.setOnMouseClicked(event -> handleFindByAddress());	
+		btnFindByName.setOnMouseClicked(event -> handleFindByName());
+		
+		loadMemberTableView();
+	}
+		
+	private void showMemberInfo(Member member) {
+		if (member != null) {
+			tfEmail.setText(member.getEmail());
+			tfPw.setText(member.getPw());
+			tfName.setText(member.getName());
+			tfBirth.setText(member.getBirth());
+			tfAddress.setText(member.getAddress());
+			tfContact.setText(member.getContact());
+			
+			if(member.getGender().equals("남자"))
+				male.setSelected(true);
+			else
+				female.setSelected(true);
+			/*
+			 * if(member.getGender().equals("남자"))
+				System.out.println(" MALE True");
+			elsemale.setSelected(true);
+			female.setSelected(true);
+				System.out.println(member.getGender());
+			 */
+		}
+		else {
+			
+		}
+	}
+	
+	private void loadMemberTableView() {
+		memberList = memberService.readList();
+		for(Member m : memberList) {
+			data.add(m);
+		}
+		tableViewMember.setItems(data);
+	}
+	
+	private boolean checkValidForm() {
+		if(tfEmail.getText().length() > 0 && tfPw.getText().length() > 0 && tfName.getText().length() > 0 && tfBirth.getText().length() > 0)
+			return true;
+		if(tfEmail.getText().length() < 0 || !tfEmail.getText().contains("@")) {
+			this.showAlert("Email를 확인하십시요");
+			return false;
+		}
+		return false;
+	}
+private String ageCheck() { 
+		
+		Calendar cal = Calendar.getInstance();
+		int age1 = Integer.parseInt(tfBirth.getText().substring(0,4));  // 입력된 년도 
+		int age2 = Integer.parseInt(tfBirth.getText().substring(4,8));  //입력된 월, 일 
+		int year = cal.get(Calendar.YEAR);
+		int month = (cal.get(Calendar.MONTH)+1)*100;  // 현재 월 뽑아서 100곱해서  
+		int date = cal.get(Calendar.DATE);  // 현재 일 
+		int DB = month + date;  // 현재 월,일 합침  
+	
+		if ( age2 >= DB  ) {   // 생일 지남 
+			
+			return Integer.toString(year - age1-1);
+		} else {    // 생일 안 지남 
+			return Integer.toString(year - age1); 
+		}
+		
+	}
+	private String gender()
+	{
+		if(male.isSelected())
+			return "남자";
+		else
+			return "여자";
+	}
+	@FXML 
+	private void handleFindByAddress() {
+		String condition = tfFindCondition.getText();
+		taFindResult.setText("");
+		if(condition.length() > 0) {
+			List<Member> searched = memberService.findByAddress(condition);
+			if(searched.size() > 0) {
+				int no = 1;
+				for(Member m : searched) {
+					taFindResult.appendText(no++ + " ) " + m.getAddress() + " : " + m.getEmail() + " : " + m.getName() + " \n");
+				}
+			}
+			else
+				taFindResult.setText("검색 조건에 맞는 정보가 없습니다.");
+		}
+		else
+			this.showAlert("검색 조건을 입력하십시요");			
+	}
+	
+	@FXML 
+	private void handleFindByName() {
+		String condition = tfFindCondition.getText();
+		taFindResult.setText("");
+		if(condition.length() > 0) {
+			List<Member> searched = memberService.findByName(condition);
+			if(searched.size() > 0) {
+				int no = 1;
+				for(Member m : searched) {
+					taFindResult.appendText(no++ + " ) " + m.getAddress() + " : " + m.getEmail() + " : " + m.getName() + " \n");
+				}
+			}
+			else
+				taFindResult.setText("검색 조건에 맞는 정보가 없습니다.");
+		}
+		else
+			this.showAlert("검색 조건을 입력하십시요");			
+	}
+	
+	@FXML 
+	private void handleCreate() { // event source, listener, handler
+		if(gender().equals("남자"))
+			System.out.println("남자");
+		else
+			System.out.println("여자");
+		
+		Member newMember = new Member(tfEmail.getText(), tfPw.getText(), tfName.getText(), 
+				tfBirth.getText(), ageCheck(), tfAddress.getText(), tfContact.getText(), gender());
+
+		int selectedIndex = tableViewMember.getSelectionModel().getSelectedIndex();
+		
+		/* 무결성 검사 */
+		if (selectedIndex == memberService.findByUid(newMember)) {
+			showAlert("중복된 이메일");    
+		}
+		
+		else if (selectedIndex >= 0) {
+			System.out.println(tableViewMember.getItems().get(selectedIndex));
+			data.add(newMember);
+			tableViewMember.setItems(data);
+			memberService.create(newMember);			
+		} else {
+			showAlert("수정 실패");          
+        }
+	}
+	
+	@FXML 
+	private void handleUpdate() {
+		Member newMember = new Member(tfEmail.getText(), tfPw.getText(), tfName.getText(), 
+				tfBirth.getText(), ageCheck(), tfAddress.getText(), tfContact.getText(), gender());
+
+		int selectedIndex = tableViewMember.getSelectionModel().getSelectedIndex();
+		
+		/* 무결성 검사 */
+		if (selectedIndex != memberService.findByUid(newMember)) {
+			showAlert("이메일을 수정하면 업데이트 할 수 없습니다.");    
+		}
+		
+		else if (selectedIndex >= 0) {
+			System.out.println(tableViewMember.getItems().get(selectedIndex));
+			tableViewMember.getItems().set(selectedIndex, newMember);
+			memberService.update(newMember);			
+		} else {
+			showAlert("수정 실패");          
+        }
+	}	
+	
+	@FXML 
+	private void handleDelete() {
+		int selectedIndex = tableViewMember.getSelectionModel().getSelectedIndex();
+		if (selectedIndex >= 0) {
+			memberService.delete(tableViewMember.getItems().remove(selectedIndex));			
+		} else {
+			showAlert("삭제 실패");
+        }
+	}
+	
+	private void showAlert(String message) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+        alert.initOwner(mainApp.getRootStage());
+        alert.setTitle("알림");
+        alert.setContentText("확인 : " + message);            
+        alert.showAndWait();
+	}
+
+	private Main mainApp;
+
+    public void setMainApp(Main mainApp) {
+        this.mainApp = mainApp;
+    }
+
+}
